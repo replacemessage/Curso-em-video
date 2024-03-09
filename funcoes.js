c917cf099e2574e201950a93425670534dd3f9bb
REPLACEMESSAGE <40868315+replacemessage@users.noreply.github.com>
2024-03-09 13:35:37 -0300

    page login 1.0
    Finalizado a pagina de login com todos efeitos gráfico e algumas melhorias

--- funcoes.js
+++ funcoes.js
@@ -1,21 +1,23 @@
 
-    var a = document.getElementById("loginBtn");
-    var b = document.getElementById("registerBtn");
-    var x = document.getElementById("card-login");
-    var y = document.getElementById("card-cadastro");
+    /*Pega a identificação dos botões*/
+var a = document.getElementById("loginBtn");
+var b = document.getElementById("registerBtn");
+    /*Pega a identificação das div's "card-"*/
+var x = document.getElementById("k-login");
+var y = document.getElementById("k-cadastro");
+    
+function login() {
+     x.style.left = "4px";        
+     y.style.right = "-520px";
 
-    function login()
-    {
-        x.style.left = "4px";
-    y.style.right="-520px";
-    a.className +="white-btn";
-    b.className = "btn";
-    }
-    function register()
-    {
-        x.style.left = "-510px";
-    y.style.right="-5px";
-    a.className +="btn";
-    b.className = "white-btn";
-    }
+     a.className += "white-btn";
+     b.className = "btn";
+}
+function register() {   
+     x.style.left = "-510px"; 
+     y.style.right = "-4px";
+
+     a.className += "btn";
+     b.className = "white-btn";
+}
 
